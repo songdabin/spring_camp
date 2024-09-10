08919defde13d6f316559128b88b581518112064
Songdabin <thdekqls1222@naver.com>
2024-09-10 23:02:56 +0900

    chore: delete comment

--- src/main/java/com/example/demo/controller/BoardRestController.java
+++ src/main/java/com/example/demo/controller/BoardRestController.java
@@ -24,75 +24,21 @@ public class BoardRestController {
     List<Map<String, Object>> boardList = new ArrayList<>();
     @GetMapping("/create")
     public Map<String, Object> create(@RequestParam Map<String, Object> params) {
-
         return boardService.createBoard(params);
-        /*
-        컨트롤러 필드에 저장하는 코드 (DB 연결 전)
-//        아래의 두 가지 string 변환 방법 모두 사용 가능
-        String title = (String) params.get("title");
-        String content = params.get("content")+"";
-        String author = params.get("author")+"";
-        int order = boardList.size();
-
-        Map<String, Object> boardMap = new HashMap<>();
-        boardMap.put("order", ++order);
-        boardMap.put("title", title);
-        boardMap.put("content", content);
-        boardMap.put("author", author);
-        boardList.add(boardMap);
-
-        System.out.println(boardList);
-
-        Map<String,Object> resultMap = new HashMap<>();
-        resultMap.put("resultCode", 200);
-        */
     }
 
     @GetMapping("/list")
     public List<Board> list() {
-        /*
-        DB 연결 전 코드
-        return boardList;
-        */
         return boardService.listBoard();
     }
 
     @GetMapping("/detail")
     public Board detail(@RequestParam Integer id) {
         return boardService.detailBoard(id);
-
-        /*
-        int index = Integer.parseInt(order) - 1;
-
-        Map<String, Object> boardMap = boardList.get(index);
-
-        Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("result_code", 200);
-        resultMap.put("detail_board", boardMap);
-
-        return resultMap;
-         */
     }
 
     @GetMapping("/update")
     public Map<String, Object> update(@RequestParam Map<String, Object> params) {
-        /*
-        DB 연결 전
-
-        int index = Integer.parseInt(params.get("order") + "") - 1;
-
-        Map<String, Object> boardMap = boardList.get(index);
-        boardMap.put("title", params.get("title"));
-        boardMap.put("content", params.get("content"));
-        boardMap.put("author", params.get("author"));
-//        boardList.add()
-
-        Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("result_code", 200);
-//        resultMap.put("detail_board", boardMap);
-
-        return resultMap;
-         */
         return boardService.updateBoard(params);
     }
 
